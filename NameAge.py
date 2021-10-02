c9881196c883fe7565c4937ff138e3106d66b9b1
wambui-wainaina <wambui.8.wainaina@gmail.com>
2021-10-02 12:46:32 +0300

    second commit

---
+++ NameAge.py
@@ -0,0 +1,4 @@
+name = input ("Hey, what's your name? ")
+age = int(input("How old are you? "))
+year = str((2021 - age)+100)
+print ("Hello " +name+ " You will turn 100 in the year" ,year)
---
+++ TempConverter.py
@@ -0,0 +1,5 @@
+temp = float(input("Enter temperature in Fahrenheit: "))
+
+celsius = (temp - 32)/1.8
+
+print(f"{temp} in Fahrenheit is equal to {celsius} degrees in Celsius")
